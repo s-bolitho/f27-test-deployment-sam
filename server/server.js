08c6f66c7ada19a018932fed3d94612191be1d6b
s-bolitho <sambolitho4@gmail.com>
2022-08-31 13:57:17 -0500

    update

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../client/index.html"))
 });
 
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/styles.css"))
+})
+
 const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
