cd920eb3227236695d9ef5c1e5e5df19f5e76019
s-bolitho <sambolitho4@gmail.com>
2022-08-31 13:29:52 -0500

    update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 
+
 const app = express();
 
 //endpoints
@@ -8,7 +9,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../client/index.html"))
 });
 
-const port = 4005;
+const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
