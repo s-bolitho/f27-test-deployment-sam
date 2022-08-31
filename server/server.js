a2f755a62f4ddac325828b2deff23b352910c4ae
s-bolitho <sambolitho4@gmail.com>
2022-08-31 13:06:01 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//endpoints
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/index.html"))
+});
+
+const port = 4005;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
